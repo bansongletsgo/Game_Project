58b6b2464e4d81bcc8b1652d8b1c9a0c9298cf75
hanryang <mssh02@konkuk.ac.kr>
2024-06-28 17:06:18 +0900

    임시 저장

---
+++ Assets/Script/People/People.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class People : MonoBehaviour
+{
+    protected float walkingspeed;
+    protected float runningspeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkingspeed = 1f;
+        runningspeed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
