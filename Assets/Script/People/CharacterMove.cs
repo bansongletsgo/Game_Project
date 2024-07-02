3cf65c0e994871ed1ed890575db3ef9f15866706
jinwoo <jinwoo743@gmail.com>
2024-07-02 14:17:59 +0900

    CharacterMove

--- Assets/Script/People/CharacterMove.cs
+++ Assets/Script/People/CharacterMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class left : MonoBehaviour
+public class CharacterMove : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     public LayerMask layerMask;
