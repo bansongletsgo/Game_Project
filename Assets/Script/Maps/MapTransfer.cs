124f8342fe6020168a4882b9499a2ddf47c46acd
jinwoo <jinwoo743@gmail.com>
2024-07-17 15:28:32 +0900

    맵 이동

--- Assets/Script/Maps/MapTransfer.cs
+++ Assets/Script/Maps/MapTransfer.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class MapTransfer : MonoBehaviour
 {
     public string transferMapName; //이동할 맵의 이름
+    public Transform target;
     private CharacterMove thePlayer;
+    private CameraMove theCamera;
     // Start is called before the first frame update
     void Start()
     {
+        theCamera = FindObjectOfType<CameraMove>();
         thePlayer = FindObjectOfType<CharacterMove>();
     }
 
@@ -17,7 +20,9 @@ public class MapTransfer : MonoBehaviour
     {
         if(collision.gameObject.name == "Player"){
             thePlayer.currentMapName = transferMapName;
-            SceneManager.LoadScene(transferMapName);
+            //SceneManager.LoadScene(transferMapName);
+            theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
+            thePlayer.transform.position = target.transform.position;
         }
     }
 }
